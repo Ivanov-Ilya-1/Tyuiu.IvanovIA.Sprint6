5acd35648765077ab04bbd71709d579e70e32bb4
tyuiu.ivanovia@outlook.com <gilya0407@gmail.com>
2025-12-09 16:41:35 +0300

    Add project files.

---
+++ Tyuiu.IvanovIA.Sprint6.Task0.V28.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.IvanovIA.Sprint6.Task0.V28.Lib
+{
+    public class DataService : ISprint6Task0V28
+    {
+        public double Calculate(int x)
+        {
+            double y = (6.1 * Math.Pow(x,3)) + (0.23 * Math.Pow(x, 2)) + (1.04 * x);
+
+            double result = Math.Round(y, 3);         
+
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.IvanovIA.Sprint6.Task0.V28/FormMain.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.IvanovIA.Sprint6.Task0.V28.Lib;
+
+namespace Tyuiu.IvanovIA.Sprint6.Task0.V28
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBoxInfo_AY_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelInfo_AY_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxAnswer_AY_TextChanged(object sender, EventArgs e)
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            double res = ds.Calculate(x);
+            textBoxAnswer_AY.Text = res.ToString();
+        }
+    }
+}
