8f8a031cab53bb61faab829af7facfdfc9383207
tyuiu.ivanovia@outlook.com <gilya0407@gmail.com>
2025-12-11 18:44:51 +0300

    Task5

--- Tyuiu.IvanovIA.Sprint6.Task5.V3.Lib/DataService.cs
+++ Tyuiu.IvanovIA.Sprint6.Task5.V3.Lib/DataService.cs
@@ -8,41 +8,29 @@ namespace Tyuiu.IvanovIA.Sprint6.Task5.V3.Lib
 
         public double[] LoadFromDataFile(string path)
         {
-            List<double> realNumbers = new List<double>();
-
+            len = 0;
             using (StreamReader reader = new StreamReader(path))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    // Разбиваем строку на части (поддерживаем разные разделители)
-                    string[] parts = line.Split(new[] { ' ', '\t', ',', ';' },
-                        StringSplitOptions.RemoveEmptyEntries);
-
-                    foreach (string part in parts)
-                    {
-                        // Пробуем преобразовать в double (поддерживаем разные форматы)
-                        if (double.TryParse(part.Replace(',', '.'),
-                            NumberStyles.Any,
-                            CultureInfo.InvariantCulture,
-                            out double number))
-                        {
-                            // Проверяем, что это вещественное число (имеет дробную часть)
-                            // И не является специальными значениями
-                            if (!double.IsInfinity(number) &&
-                                !double.IsNaN(number) &&
-                                Math.Abs(number % 1) > 0.0000000001) // Эпсилон-проверка
-                            {
-                                // Округляем до 3 знаков после запятой
-                                double rounded = Math.Round(number, 3);
-                                realNumbers.Add(rounded);
-                            }
-                        }
-                    }
+                    len++;
                 }
             }
 
-            return realNumbers.ToArray();
+            double[] res = new double[len];
+            int i = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    res[i] = Math.Round(Convert.ToDouble(line), 3);
+                    i++;
+                }
+            }
+            res = res.Where(x => Math.Abs(x % 10) != 0).ToArray();
+            return res;
         }
     }
 }
