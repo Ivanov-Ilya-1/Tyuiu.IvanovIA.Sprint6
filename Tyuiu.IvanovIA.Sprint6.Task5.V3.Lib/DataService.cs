90577921c2d6b4595c8f6129ee0f78b1267e98da
tyuiu.ivanovia@outlook.com <gilya0407@gmail.com>
2025-12-11 18:39:47 +0300

    Task5

--- Tyuiu.IvanovIA.Sprint6.Task5.V3.Lib/DataService.cs
+++ Tyuiu.IvanovIA.Sprint6.Task5.V3.Lib/DataService.cs
@@ -1,4 +1,5 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint6;
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint6;
 namespace Tyuiu.IvanovIA.Sprint6.Task5.V3.Lib
 {
     public class DataService : ISprint6Task5V3
@@ -7,30 +8,41 @@ namespace Tyuiu.IvanovIA.Sprint6.Task5.V3.Lib
 
         public double[] LoadFromDataFile(string path)
         {
-            int len = 0;
-            int index = 0;
+            List<double> realNumbers = new List<double>();
+
             using (StreamReader reader = new StreamReader(path))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    len++;
+                    // Разбиваем строку на части (поддерживаем разные разделители)
+                    string[] parts = line.Split(new[] { ' ', '\t', ',', ';' },
+                        StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (string part in parts)
+                    {
+                        // Пробуем преобразовать в double (поддерживаем разные форматы)
+                        if (double.TryParse(part.Replace(',', '.'),
+                            NumberStyles.Any,
+                            CultureInfo.InvariantCulture,
+                            out double number))
+                        {
+                            // Проверяем, что это вещественное число (имеет дробную часть)
+                            // И не является специальными значениями
+                            if (!double.IsInfinity(number) &&
+                                !double.IsNaN(number) &&
+                                Math.Abs(number % 1) > 0.0000000001) // Эпсилон-проверка
+                            {
+                                // Округляем до 3 знаков после запятой
+                                double rounded = Math.Round(number, 3);
+                                realNumbers.Add(rounded);
+                            }
+                        }
+                    }
                 }
             }
 
-            double[] numsArray = new double[len];
-            using (StreamReader reader = new StreamReader(path))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    numsArray[index++] = Convert.ToDouble(line);
-                }
-            }
-
-            numsArray = numsArray.Where(val => val % 10 != 0).ToArray();
-
-            return numsArray;
+            return realNumbers.ToArray();
         }
     }
 }
